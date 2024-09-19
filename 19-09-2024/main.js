39f64eff48165320babe799ee17dc45c991a47cc
Mikołaj Rojczyk <89742061+r0jooo@users.noreply.github.com>
2024-09-19 14:03:53 +0000

    new file:   19-09-2024/main.js

---
+++ 19-09-2024/main.js
@@ -0,0 +1,11 @@
+// 3.1 s.65
+
+zakupy = new Array("Mleko", "Pieczywo", "Jabłka")
+
+console.log(zakupy.length)
+        
+zakupy[1] = "Banan"
+        
+zakupy.forEach(element => {
+    console.log(element + " ");
+});
