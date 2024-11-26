d1a66fc887b43ce1450ce457dcc958f8b2ae6ba1
Mikołaj Rojczyk <89742061+r0jooo@users.noreply.github.com>
2024-11-26 15:34:11 +0000

    zad. 3.4 s. 74

--- 2024/Nov/26-11.js
+++ 2024/Nov/26-11.js
@@ -26,3 +26,24 @@ write(pies.imie);
 write(pies.waga);
 
 pies.szczekaj();
+
+// Zad. 3.4 s.74
+
+let myCar = {
+    make: 'Volkswagen',
+    model: 'Passat',
+    edition: 'B5',
+    year: 2003,
+    color: 'black',
+    isElectric: false
+};
+
+let color = 'blue';
+myCar['color'] = color;
+
+myCar['forSale'] = true;
+
+console.log(`Marka samochodu: ${myCar.make}`);
+console.log(`Model samochodu: ${myCar.model}`);
+
+console.log(`Na sprzedaż: ${myCar['forSale']}`);
