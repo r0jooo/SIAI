41bad7faad51b5a7a399c2abba2498a19bd3e092
Mikołaj Rojczyk <89742061+r0jooo@users.noreply.github.com>
2024-11-28 18:25:05 +0000

    Refactor: Remove zadania.js and split content into separate files for better organization

---
+++ 2024/Zadania/Nov/3.3-71.js
@@ -0,0 +1,5 @@
+// 3.3/71
+
+const array = [1, 2, 3];
+const newArray = [array, array, array];
+console.log(newArray[1][1]);
---
+++ 2024/Zadania/Nov/4.1-82.js
@@ -0,0 +1,11 @@
+// 4.1/82
+
+let bool = false;
+
+console.log(bool);
+
+if (bool) {
+    console.log("true");
+} else {
+    console.log("false");
+}
