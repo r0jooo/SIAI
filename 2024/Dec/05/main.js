3b53ec14cafdfe44c63c8fb2c74b7df6af53fe52
Mikołaj Rojczyk <89742061+r0jooo@users.noreply.github.com>
2024-12-05 14:24:12 +0000

    Add main.js for distance conversion from miles to kilometers

---
+++ 2024/Dec/05/main.js
@@ -0,0 +1,8 @@
+// Zmienna przechowująca wartość w milach
+let miles = 200;
+
+// Konwersja mil na kilometry (1 mila = 1.60934 kilometra)
+let kilometers = miles * 1.60934;
+
+// Wyświetlenie wyniku w konsoli
+console.log(`Odległość ${miles} mil odpowiada ${kilometers.toFixed(4)} kilometra`);
