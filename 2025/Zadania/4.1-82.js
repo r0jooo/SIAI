bb981840ce63ef1aa0593560c782083821a6d9d4
Mikołaj Rojczyk <89742061+r0jooo@users.noreply.github.com>
2025-01-20 20:49:06 +0000

    Zadania z czwartego rozdziału

---
+++ 2025/Zadania/4.1-82.js
@@ -0,0 +1,16 @@
+// 4.1/82
+
+let bool = false;
+
+console.log(bool);
+
+if (bool) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+if (!bool) {
+    console.log("The condition is false");
+}
+
+bool = !bool;
